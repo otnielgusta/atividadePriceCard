064ef0d64f47bddf306e35a8e1042774d4fbc6be
otnielgusta <otnielsilvag4@gmail.com>
2022-03-17 09:26:57 -0300

    desgraça

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PlanoComponnent from './lib/plano_componnent';
 
 export default function App() {
+
+  const planos= [
+    {
+      nome: 'Plano A',
+      preco: 'R$ 20,00',
+      descricao: ['Leve', 'Barato', 'Bacana']
+    },
+    
+    {
+      nome: 'Plano B',
+      preco: 'R$ 30,00',
+      descricao: ['Ótimo custo-benefício']
+    },
+    {
+      nome: 'Plano C',
+      preco: 'R$ 40,00',
+      descricao: ['Sempre conectado', 'Jogos', 'Vídeo']
+    },
+    {
+      nome: 'Plano D',
+      preco: 'R$ 50,00',
+      descricao: ['Assistência premium', 'Cashback']
+    },
+    
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Planos</Text>
+      {
+        planos.map((data) =>{ return <PlanoComponnent plano={data} />})
+      }
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#87ceeb',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  title:{
+    fontSize:32,
+    marginTop:30
+  }
 });
