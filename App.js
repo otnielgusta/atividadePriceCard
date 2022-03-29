9909afa264f062a7ecce9162ee67c3daf1bc0f71
otnielgusta <otnielsilvag4@gmail.com>
2022-03-29 06:41:37 -0300

    introducao a usestate

--- App.js
+++ App.js
@@ -1,53 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import PlanoComponnent from './lib/plano_componnent';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Janela2 from './lib/janelas/janela2/janela2';
+import Janela3 from './lib/janelas/janela3/janela3';
+import Produtos from './lib/janelas/produtos/produtos';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 
-  const planos= [
-    {
-      nome: 'Plano A',
-      preco: 'R$ 20,00',
-      descricao: ['Leve', 'Barato', 'Bacana']
-    },
-    
-    {
-      nome: 'Plano B',
-      preco: 'R$ 30,00',
-      descricao: ['Ótimo custo-benefício']
-    },
-    {
-      nome: 'Plano C',
-      preco: 'R$ 40,00',
-      descricao: ['Sempre conectado', 'Jogos', 'Vídeo']
-    },
-    {
-      nome: 'Plano D',
-      preco: 'R$ 50,00',
-      descricao: ['Assistência premium', 'Cashback']
-    },
-    
-  ]
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Planos</Text>
-      {
-        planos.map((data) =>{ return <PlanoComponnent plano={data} />})
-      }
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Produtos"
+          component={Produtos}
+          options={{
+            title:"Produtos",
+            headerStyle:{
+              backgroundColor: '#633BBC',              
+            },
+            headerTintColor:"#fff",
+            headerTitleAlign:'center',
+            headerTitleStyle:{
+
+              fontWeight:'bold'
+            },
+            headerShadowVisible: false
+            
+          }}
+          
+        />
+  
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#87ceeb',
-    alignItems: 'center',
-    
-  },
-  title:{
-    fontSize:32,
-    marginTop:30
-  }
-});
